682779027edeceea85542dc1dc38f850838ef869
Alex4386 <psh010209@gmail.com>
2019-07-10 16:03:24 +0900

    add: archiving script

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo ==================================================
+echo ' _____                  ___   __  __ '
+echo '|_   _|   _ _ __ _ __  / _ \ / _|/ _|'
+echo "  | || | | | '__| '_ \| | | | |_| |_ "
+echo '  | || |_| | |  | | | | |_| |  _|  _|'
+echo '  |_| \__,_|_|  |_| |_|\___/|_| |_|  '
+echo                                     
+echo ' _   _                     __        ___ _    _ '
+echo '| \ | | __ _ _ __ ___  _   \ \      / (_) | _(_)'
+echo "|  \| |/ _\` | '_ \` _ \\| | | \\ \\ /\\ / /| | |/ / |"
+echo '| |\  | (_| | | | | | | |_| |\ V  V / | |   <| |'
+echo '|_| \_|\__,_|_| |_| |_|\__,_| \_/\_/  |_|_|\_\_|'
+                                                
+echo ==================================================
+echo Package Builder
+echo ==================================================
+echo
+paus
+
+echo Current Working Directory is...
+pwd
+echo
+
+echo Removing existing TurnOff-NamuWiki Archive.
+rm turnoff-namuwiki.zip
+echo Done.
+echo
+
+echo Building TurnOff-NamuWiki Archive....
+zip -r turnoff-namuwiki.zip * --exclude=*.git* --exclude=*node_modules*
+echo
