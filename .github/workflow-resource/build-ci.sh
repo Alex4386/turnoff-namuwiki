722c5da293c65dd2b801e911cc763be13121fb44
박상희 (Alex Park) <psh010209@gmail.com>
2020-05-06 07:07:12 +0900

    add: ci builds

---
+++ .github/workflow-resource/build-ci.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo ==================================================
+echo ' _____                  ___   __  __ '
+echo '|_   _|   _ _ __ _ __  / _ \ / _|/ _|'
+echo "  | || | | | '__| '_ \| | | | |_| |_ "
+echo '  | || |_| | |  | | | | |_| |  _|  _|'
+echo '  |_| \__,_|_|  |_| |_|\___/|_| |_|  '
+echo                                     
+echo ' _   _                     __        ___ _    _ '
+echo '| \ | | __ _ _ __ ___  _   \ \      / (_) | _(_)'
+echo "|  \| |/ _\` | '_ \` _ \\| | | \\ \\ /\\ / /| | |/ / |"
+echo '| |\  | (_| | | | | | | |_| |\ V  V / | |   <| |'
+echo '|_| \_|\__,_|_| |_| |_|\__,_| \_/\_/  |_|_|\_\_|'
+                                                
+echo ==================================================
+echo Package Builder
+echo ==================================================
+echo
+sleep 3
+
+echo Current Working Directory is...
+pwd
+echo
+
+echo Removing existing TurnOff-NamuWiki Archive.
+rm turnoff-namuwiki.zip
+echo Done.
+echo
+
+echo Adding CI Build Information
+git rev-parse --short HEAD > ci_build_ver.txt
+
+echo Building TurnOff-NamuWiki Archive....
+zip -r turnoff-namuwiki.zip * --exclude=*.git* --exclude=*node_modules* --exclude=*showcase/marketplace/* --exclude=*.DS_store --exclude=*.sh --exclude=*intelliBan/* --exclude=*showcase/marketplace* --exclude=*showcase/logo.psd
+
+echo Removing Production Build Information
+rm ci_build_ver.txt
+echo
