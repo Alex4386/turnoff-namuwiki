42fb0be3ee44cb88592229300cd11b565c4dd41a
Kagami Sascha Rosylight <saschanaz@outlook.com>
2022-05-03 03:01:30 +0200

    이미지 검색 셀렉터

--- src/filter/duckduckgo.ts
+++ src/filter/duckduckgo.ts
@@ -5,7 +5,7 @@
     const urlRegex = "^http(s?):\\/\\/";
     const blockRules = [ "namu.wiki", "namu.mirror.wiki", "namu.moe", "mir.pe", "namu.news" ];
 
-    const searchResultClasses = [ 'nrn-react-div' ];
+    const searchResultClasses = [ 'nrn-react-div', 'tile' ];
 
     let searchResults: HTMLDivElement[] = [];
 
